6d14f49dae9c8087839276858e18161713a5eb1e
nguyenvukhang <brew4k@gmail.com>
2025-02-09 10:40:15 +0800

    Update homepage

--- generate.py
+++ generate.py
@@ -3,12 +3,7 @@ import sys
 
 z = {}
 
-z["index"] = "https://github.com/libmath/math/issues/1"
-z["wiki"] = "https://github.com/libmath/z/wiki"
-z["stack"] = "https://github.com/libmath/math/issues/1"
-z["mods"] = (
-    "https://docs.google.com/spreadsheets/d/1_6HvZEyN8qOcKPjzuB2e7hyQtPOQKUUcr2r0WQdD99k/edit?gid=1740291173"
-)
+z["food"] = "https://azmobileapp.azurewebsites.net/stuffd/tabs/home?method=1&locationId=b805a6f0-0341-4f18-a7cf-b428568f666e&q=s"
 
 for slug, url in z.items():
     with open(slug + ".html", "w") as sys.stdout:
